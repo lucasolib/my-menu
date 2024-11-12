f8aff74fc0ce05f4214dce0239657a522cab05a3
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2024-11-12 01:18:16 -0300

    Feat: Nova página de Meu Menu

--- src/header.tsx
+++ src/header.tsx
@@ -1,15 +1,24 @@
+import { Link } from 'react-router-dom';
 import './css/header.css';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="myMenuLogo">Meu menu</h1>
-      <div className="headerInteractables">
-        <h3 className="home">Inicio</h3>
-        <h3 className="myMenu">Meu cardápio</h3>
+      <nav className="headerInteractables">
+        <h3 className="home">
+          <Link to="/">
+            Inicio
+          </Link>
+        </h3>
+        <h3 className="myMenu">
+          <Link to="myMenu">
+            Meu cardápio
+          </Link>
+        </h3>
         <h3 className="onlineMenu">Cardápio online</h3>
         <h3 className="contact">Contatos</h3>
-      </div>
+      </nav>
     </header>
   );
 }
---
+++ src/myMenu.tsx
@@ -0,0 +1,14 @@
+import './css/myMenu.css';
+import DishCard from './dishCard';
+import Header from './header';
+
+function myMenu() {
+  return (
+    <main>
+      <Header />
+      <DishCard />
+    </main>
+  );
+}
+
+export default myMenu;
